f4d42f934b5d28adf7bb71b934b1d77001dadc3d
Azamat-V-U <tester_01_980@mail.ru>
2024-05-11 14:37:46 +0300

    Azamat_V_homework_4_done

---
+++ homework/azamat_valiullin/homework_4/basics_2.py
@@ -0,0 +1,18 @@
+my_dict = {
+    "tuple": (1, 2, 3, 4, 5),
+    "list": [1, True, "car", 2.35, 10],
+    "dict": {"maker": "bmw", "model": "550i", "color": "black", "year": "2010", "condition": "not broken"},
+    "set": {3, False, "cat", 3.35, 20}
+}
+print(my_dict["tuple"][-1])
+
+my_dict["list"].append("test")
+my_dict["list"].pop(1)
+
+my_dict["dict"]["I am a tuple"] = 6
+del my_dict["dict"]["color"]
+
+my_dict["set"].add("new element")
+my_dict["set"].remove("cat")
+print(my_dict)
+
