4c90ced378a99dc480e563078dd19623d3ddf1e6
Azamat-V-U <tester_01_980@mail.ru>
2024-05-24 12:56:51 +0300

    Azamat_V_homework_9_lint_correction

--- homework/azamat_valiullin/homework_9/date_processing.py
+++ homework/azamat_valiullin/homework_9/date_processing.py
@@ -1,12 +1,15 @@
 import datetime
 
+
 given_date = "Jan 15, 2023 - 12:05:33"
+
 python_date = datetime.datetime.strptime(given_date, "Jan %d, %Y - %H:%M:%S")
 print(python_date)
+
 
 month = python_date.strftime("%B")
 print(month)
 
-human_date = python_date.strftime(f"%d.%m.%Y, %I:%M ")
-pm = python_date.strftime("%p").lower()
-print(f"{human_date}{pm}")
+
+human_date = python_date.strftime(f"%d.%m.%Y, %I:%M %p ")
+print(human_date)
