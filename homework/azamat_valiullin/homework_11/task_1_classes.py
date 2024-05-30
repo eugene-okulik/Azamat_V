10a556b51aa42452fcbca0f0b95c9628cf1c550c
Azamat-V-U <tester_01_980@mail.ru>
2024-05-30 13:09:48 +0300

    Azamat_V_homework_11_lint__correction

--- homework/azamat_valiullin/homework_11/task_1_classes.py
+++ homework/azamat_valiullin/homework_11/task_1_classes.py
@@ -10,11 +10,11 @@ class Book:
         self.availability = availability
 
 
-book_1 = Book("Idiot", "Dostoevsky", 500, 978 - 5 - 7320 - 1162 - 3)
-book_2 = Book("The Great Gatsby", "F. Scott Fitzgerald", 700, 978 - 5 - 4820 - 1162 - 3)
-book_3 = Book("Uncle Tom's Cabin", "Harriet Elizabeth Beecher Stowe", 435, 858 - 5 - 7320 - 11624)
-book_4 = Book("Dead Souls", "Nikolai Gogol", 850, 978 - 5 - 7320 - 8962 - 3, True)
-book_5 = Book("Anna Karenina", "Leo Tolstoy", 900, 898 - 5 - 7320 - 1162 - 3)
+book_1 = Book("Idiot", "Dostoevsky", 500, "978-5-7320-1162-3")
+book_2 = Book("The Great Gatsby", "F. Scott Fitzgerald", 700, "555-5-7820-1162-3")
+book_3 = Book("Uncle Tom's Cabin", "Harriet Elizabeth Beecher Stowe", 515, "118-5-7320-8262-3")
+book_4 = Book("Dead Souls", "Nikolai Gogol", 850, "901-5-4420-1162-3", True)
+book_5 = Book("Anna Karenina", "Leo Tolstoy", 900, "888-5-7120-1162-3")
 
 book_list = [book_1, book_2, book_3, book_4, book_5]
 
@@ -38,17 +38,17 @@ class SchoolBooks(Book):
 
 
 school_book_1 = SchoolBooks(
-    "Algebra", "Ivanov", 350, 978-5-7320-1162-3, "Mathematics",
-    "9", True, True
+    "Algebra", "Ivanov", 350, "978-5-7320-1162-3", "Mathematics",
+    9, True, True
 )
 
 school_book_2 = SchoolBooks(
-    "History", "Sidorov", 450, 111-5-7320-1162-3, "History",
+    "History", "Sidorov", 450, "111-5-7320-1162-3", "History",
     9, True
 )
 
 school_book_3 = SchoolBooks(
-    "Biology", "Petrov", 280, 978-5-7320-1162-3, "Biology",
+    "Biology", "Petrov", 280, "978-5-7320-1162-3", "Biology",
     9, True
 )
 
