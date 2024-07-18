c7182296ccb0f2b7580e6d0cb3c779a224595c5d
Azamat-V-U <tester_01_980@mail.ru>
2024-07-18 17:15:46 +0300

    Added_code_to_fill_subjects_and_date_of_birth_fields

--- homework/azamat_valiullin/homework_23/test_task_2.py
+++ homework/azamat_valiullin/homework_23/test_task_2.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from faker import Faker
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
@@ -77,13 +79,25 @@ def test_form_filling_verification(driver):
     send_keys(driver, fake.email(), "(//input[contains(@id, 'userEmail')])", "xpath")
     element_click(driver, "//label[@for='gender-radio-1']", "xpath")
     send_keys(driver, "7982555777", "(//input[contains(@id, 'userNumber')])", "xpath")
+
     element_click(driver, "dateOfBirthInput", "id")
-    element_click(driver, "(//div[@aria-label='Choose Tuesday, July 16th, 2024'])", "xpath")
+    element = driver.find_element(By.CLASS_NAME, "react-datepicker__year-select")
+    selector = Select(element)
+    selector.select_by_value("1990")
+    element = driver.find_element(By.CLASS_NAME, "react-datepicker__month-select")
+    selector = Select(element)
+    selector.select_by_value("5")
+    element_click(driver, "(//div[@aria-label='Choose Sunday, June 10th, 1990'])", "xpath")
+
+    driver.execute_script("window.scrollBy(0, 300);")
     element_click(driver, "(//label[@for='hobbies-checkbox-1'])", "xpath")
 
-    send_keys(driver, "Subject Input", "subjectsInput", "id")
+    send_keys(driver, "ma", "subjectsInput", "id")
+    send_keys(driver, Keys.ENTER, "subjectsInput", "id")
+    send_keys(driver, "ph", "subjectsInput", "id")
+    send_keys(driver, Keys.ENTER, "subjectsInput", "id")
+
     send_keys(driver, fake.address(), "(//textarea[contains(@id, 'currentAddress')])", "xpath")
-    driver.execute_script("window.scrollBy(0, 700);")
 
     element_click(driver, "(//div[@class='col-md-4 col-sm-12'])[1]", "xpath")
     element_click(driver, "(//div[@id='react-select-3-option-0'])", "xpath")
@@ -91,7 +105,7 @@ def test_form_filling_verification(driver):
     element_click(driver, "(//div[@class='col-md-4 col-sm-12'])[2]", "xpath")
     element_click(driver, "(//div[@id='react-select-4-option-0'])", "xpath")
 
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located(
         (By.XPATH, "(//button[@id='submit'])")))
 
     element_click(driver, "(//button[@id='submit'])", "xpath")
